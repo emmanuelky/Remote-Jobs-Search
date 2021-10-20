b522b55b518a447e72bddeaa86797530106391f2
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-10-20 06:22:56 +0200

    fixing deployment

--- remote-job-search/src/components/MyNavBar.jsx
+++ remote-job-search/src/components/MyNavBar.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 
-
 const MyNavBar = ({ jobs }) => {
 
     const favouriteJob = useSelector(state => state.user.favourites.length)
